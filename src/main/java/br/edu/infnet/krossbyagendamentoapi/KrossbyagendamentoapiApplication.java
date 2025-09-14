5ed038602e543c50c66e0126622fda0301a2609f
Krossby <krosscaal@gmail.com>
2025-09-14 10:14:08 -0300

    Commit Inicio projeto Agendamento

---
+++ src/main/java/br/edu/infnet/krossbyagendamentoapi/KrossbyagendamentoapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.krossbyagendamentoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrossbyagendamentoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrossbyagendamentoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/krossbyagendamentoapi/KrossbyagendamentoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.krossbyagendamentoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrossbyagendamentoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
