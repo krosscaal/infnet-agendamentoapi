fe4e467181addb73404c9d547d4ae85ecb72c068
Krossby <krosscaal@gmail.com>
2025-09-19 14:10:53 -0300

    feat: implementando os métodos do serviço de Agendamento para passar nos testes do ciclo green(TDD)

--- src/main/java/br/edu/infnet/krossbyagendamentoapi/service/AgendamentoService.java
+++ src/main/java/br/edu/infnet/krossbyagendamentoapi/service/AgendamentoService.java
@@ -4,17 +4,46 @@
  */
 package br.edu.infnet.krossbyagendamentoapi.service;
 
+import br.edu.infnet.krossbyagendamentoapi.exception.BusinessException;
+import br.edu.infnet.krossbyagendamentoapi.utils.GeralUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendamentoService {
     public Boolean validarNome(String nome) {
-        return null;
+        if (nome == null) {
+            throw new BusinessException("nome não pode ser nulo");
+        }
+        if (nome.trim().isEmpty()) {
+            throw new BusinessException("nome não pode ser vazio");
+        }
+        if (GeralUtils.contemLetras(nome).equals(Boolean.FALSE)) {
+            throw new BusinessException("deve conter apenas letras");
+        }
+        return true;
     }
     public Boolean validarSobrenome(String sobrenome) {
-        return null;
+        if (sobrenome == null) {
+            throw new BusinessException("sobrenome não pode ser nulo");
+        }
+        if (sobrenome.trim().isEmpty()) {
+            throw new BusinessException("sobrenome não pode ser vazio");
+        }
+        if (GeralUtils.contemLetras(sobrenome).equals(Boolean.FALSE)) {
+            throw new BusinessException("deve conter apenas letras");
+        }
+        return true;
     }
     public Boolean validarCpf(String cpf) {
-        return null;
+        if (cpf == null) {
+            throw new BusinessException("cpf não pode ser nulo");
+        }
+        if (cpf.length() != 11) {
+            throw  new BusinessException("deve conter 11 digitos");
+        }
+        if (GeralUtils.verificarNumeroCpfValido(cpf).equals(Boolean.FALSE)) {
+            throw new BusinessException("número cpf é invalido");
+        }
+        return GeralUtils.verificarNumeroCpfValido(cpf);
     }
 }
